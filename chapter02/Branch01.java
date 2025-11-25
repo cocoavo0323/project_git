206dcf51044e4cd9561d08031c2d8d05f833e0e7
sean <cocoavo0323@gmail.com>
2025-11-25 15:09:59 +0900

    version4

--- chapter02/Branch01.java
+++ chapter02/Branch01.java
@@ -2,7 +2,7 @@
 public class Branch01{
 
     public Branch01(){
-        System.out.println("메인 브랜치에서 작업");
+        System.out.println("충돌테스트");
         System.out.println("다음 브랜치에서 작업"); 
     }
 }
